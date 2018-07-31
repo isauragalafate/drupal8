0fab2f8e93bd059725371b6499c5e8445a84e8f5
isaura.galafate <isaura.galafate@gmail.com>
2018-07-31 23:14:30 +0200

    #N/A Function CSD menu first approach.

--- web/modules/custom/csd_menu/src/Plugin/Block/MenuBlock.php
+++ web/modules/custom/csd_menu/src/Plugin/Block/MenuBlock.php
@@ -3,6 +3,8 @@
 namespace Drupal\csd_menu\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Menu\MenuLinkTreeInterface;
@@ -19,75 +21,38 @@ use Drupal\Core\Link;
  * @Block(
  *  id = "csd_menu_bock",
  *  admin_label = @Translation("Menu block"),
+ *  category = @Translation("Menus"),
+ *  deriver = "Drupal\system\Plugin\Derivative\SystemMenuBlock",
+ *  forms = {
+ *    "settings_tray" = "\Drupal\system\Form\SystemMenuOffCanvasForm",
+ *  },
  * )
  */
 class MenuBlock extends BlockBase implements ContainerFactoryPluginInterface {
-
   /**
-   * Drupal\Core\Menu\MenuLinkTreeInterface definition.
+   * The menu link tree service.
    *
    * @var \Drupal\Core\Menu\MenuLinkTreeInterface
    */
-  protected $menuLinkTree;
+  protected $menuTree;
+
   /**
-   * Drupal\Core\Menu\DefaultMenuLinkTreeManipulators definition.
-   *
-   * @var \Drupal\Core\Menu\DefaultMenuLinkTreeManipulators
-   */
-  protected $menuDefaultTreeManipulators;
-  /**
-   * Drupal\Core\Menu\MenuActiveTrailInterface definition.
-   *
-   * @var \Drupal\Core\Menu\MenuActiveTrailInterface
-   */
-  protected $menuActiveTrail;
-  /**
-   * Drupal\Core\Menu\MenuParentFormSelectorInterface definition.
-   *
-   * @var \Drupal\Core\Menu\MenuParentFormSelectorInterface
-   */
-  protected $menuParentFormSelector;
-  /**
-   * Drupal\Core\Menu\MenuTreeStorageInterface definition.
-   *
-   * @var \Drupal\Core\Menu\MenuTreeStorageInterface
-   */
-  protected $menuTreeStorage;
-  /**
-   * Drupal\Core\Menu\StaticMenuLinkOverridesInterface definition.
-   *
-   * @var \Drupal\Core\Menu\StaticMenuLinkOverridesInterface
-   */
-  protected $menuLinkStaticOverrides;
-  /**
-   * Constructs a new MenuBlock object.
+   * Constructs a new SystemMenuBlock.
    *
    * @param array $configuration
    *   A configuration array containing information about the plugin instance.
    * @param string $plugin_id
    *   The plugin_id for the plugin instance.
-   * @param string $plugin_definition
+   * @param array $plugin_definition
    *   The plugin implementation definition.
+   * @param \Drupal\Core\Menu\MenuLinkTreeInterface $menu_tree
+   *   The menu tree service.
    */
-  public function __construct(
-    array $configuration,
-    $plugin_id,
-    $plugin_definition,
-    MenuLinkTreeInterface $menu_link_tree, 
-    DefaultMenuLinkTreeManipulators $menu_default_tree_manipulators,
-    MenuActiveTrailInterface $menu_active_trail,
-    MenuParentFormSelectorInterface $menu_parent_form_selector,
-    MenuTreeStorageInterface $menu_tree_storage,
-    StaticMenuLinkOverridesInterface $menu_link_static_overrides
-  ) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MenuLinkTreeInterface $menu_tree) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->menuLinkTree = $menu_link_tree;
-    $this->menuDefaultTreeManipulators = $menu_default_tree_manipulators;
-    $this->menuActiveTrail = $menu_active_trail;
-    $this->menuParentFormSelector = $menu_parent_form_selector;
-    $this->menuTreeStorage = $menu_tree_storage;
-    $this->menuLinkStaticOverrides = $menu_link_static_overrides;
+    $this->menuTree = $menu_tree;
   }
+
   /**
    * {@inheritdoc}
    */
@@ -96,27 +61,57 @@ class MenuBlock extends BlockBase implements ContainerFactoryPluginInterface {
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('menu.link_tree'),
-      $container->get('menu.default_tree_manipulators'),
-      $container->get('menu.active_trail'),
-      $container->get('menu.parent_form_selector'),
-      $container->get('menu.tree_storage'),
-      $container->get('menu_link.static.overrides')
+      $container->get('menu.link_tree')
     );
   }
+
   /**
    * {@inheritdoc}
    */
   public function build() {
     $build = [];
     $build['csd_menu_bock']['#markup'] = 'Implement MenuBlock.';
-
+    $build['#cache'] = [
+        'contexts' => ['route']
+      ];
     $menu_name = 'main';
+    $menu_name = $this->getDerivativeId();
+    $level = 2;
+    $depth = 1;
     $menu_tree = \Drupal::menuTree();
     $parameters = $menu_tree->getCurrentRouteMenuTreeParameters($menu_name);
-    $parameters->setMinDepth(1);
-    //Delete comments to have only enabled links
-    //$parameters->onlyEnabledLinks();
+    $parameters->setMaxDepth(9);
+    $parameters->setMinDepth($level);
+    //$parameters->setMinDepth(1);
+    $parameters->onlyEnabledLinks();
+    //$parameters->addExpandedParents();
+    //$parameters->setTopLevelOnly(0);
+    if ($depth > 0) {
+      $parameters->setMaxDepth(min($level + $depth - 1, $menu_tree->maxDepth()));
+    }
+    // For menu blocks with start level greater than 1, only show menu items
+    // from the current active trail. Adjust the root according to the current
+    // position in the menu in order to determine if we can show the subtree.
+    if ($level > 1) {
+      if (count($parameters->activeTrail) >= $level) {
+        // Active trail array is child-first. Reverse it, and pull the new menu
+        // root based on the parent of the configured start level.
+        $menu_trail_ids = array_reverse(array_values($parameters->activeTrail));
+        $menu_root = $menu_trail_ids[count($menu_trail_ids)-2];
+        $menu_root_uuid = explode(':', $menu_root)[1];
+        $menu_root_entity = \Drupal::service('entity.repository')
+          ->loadEntityByUuid('menu_link_content', $menu_root_uuid);
+        $menu_root_title = $menu_root_entity->getTitle();
+        $menu_root_url = $menu_root_entity->link;
+        $parameters->setRoot($menu_root)->setMinDepth(1);
+        if ($depth > 0) {
+          $parameters->setMaxDepth(min($level - 1 + $depth - 1, $this->menuTree->maxDepth()));
+        }
+      }
+      else {
+        return [];
+      }
+    }
 
     $tree = $menu_tree->load($menu_name, $parameters);
     $manipulators = array(
@@ -124,20 +119,101 @@ class MenuBlock extends BlockBase implements ContainerFactoryPluginInterface {
       array('callable' => 'menu.default_tree_manipulators:generateIndexAndSort'),
     );
     $tree = $menu_tree->transform($tree, $manipulators);
-    $list = [];
 
-    foreach ($tree as $item) {
-      $title = $item->link->getTitle();
-      $url = $item->link->getUrlObject();
-      $list[] = Link::fromTextAndUrl($title, $url);
-    }
-
-    $build['sections'] = array(
-      '#theme' => 'item_list',
-      '#items' => $list,
-    );
-
+    $menu_root = [
+     'title' => $menu_root_title,
+     'url' => $menu_root_url
+    ];
+    $build = $menu_tree->build($tree);
+    $build['#theme'] = 'csd_menu_main';
+    $build['#menu_root'] = $menu_root;
+   $x='a';
     return $build;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheTags() {
+    // Even when the menu block renders to the empty string for a user, we want
+    // the cache tag for this menu to be set: whenever the menu is changed, this
+    // menu block must also be re-rendered for that user, because maybe a menu
+    // link that is accessible for that user has been added.
+    $cache_tags = parent::getCacheTags();
+    $cache_tags[] = 'config:system.menu.' . $this->getDerivativeId();
+    return $cache_tags;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    // ::build() uses MenuLinkTreeInterface::getCurrentRouteMenuTreeParameters()
+    // to generate menu tree parameters, and those take the active menu trail
+    // into account. Therefore, we must vary the rendered menu by the active
+    // trail of the rendered menu.
+    // Additional cache contexts, e.g. those that determine link text or
+    // accessibility of a menu, will be bubbled automatically.
+    $menu_name = $this->getDerivativeId();
+    return Cache::mergeContexts(parent::getCacheContexts(), ['route.menu_active_trails:' . $menu_name]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $config = $this->configuration;
+
+    $defaults = $this->defaultConfiguration();
+    $form['menu_levels'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Menu levels'),
+      // Open if not set to defaults.
+      '#open' => $defaults['level'] !== $config['level'] || $defaults['depth'] !== $config['depth'],
+      '#process' => [[get_class(), 'processMenuLevelParents']],
+    ];
+
+    $options = range(0, $this->menuTree->maxDepth());
+    unset($options[0]);
+
+    $form['menu_levels']['level'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Initial visibility level'),
+      '#default_value' => $config['level'],
+      '#options' => $options,
+      '#description' => $this->t('The menu is only visible if the menu item for the current page is at this level or below it. Use level 1 to always display this menu.'),
+      '#required' => TRUE,
+    ];
+
+    $options[0] = $this->t('Unlimited');
+
+    $form['menu_levels']['depth'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Number of levels to display'),
+      '#default_value' => $config['depth'],
+      '#options' => $options,
+      '#description' => $this->t('This maximum number includes the initial level.'),
+      '#required' => TRUE,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * Form API callback: Processes the menu_levels field element.
+   *
+   * Adjusts the #parents of menu_levels to save its children at the top level.
+   */
+  public static function processMenuLevelParents(&$element, FormStateInterface $form_state, &$complete_form) {
+    array_pop($element['#parents']);
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['level'] = $form_state->getValue('level');
+    $this->configuration['depth'] = $form_state->getValue('depth');
+  }
 }
